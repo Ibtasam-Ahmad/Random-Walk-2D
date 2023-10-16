c2213b37008a481a0dc36969c10c2bf29cb41b39
Ibtasam Ahmad <85065814+Ibtasam-Ahmad@users.noreply.github.com>
2023-10-16 11:33:05 +0500

    Add files via upload

---
+++ RandomWalk2D/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace RandomWalk2D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "2d Walk";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Location = new System.Drawing.Point(114, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "No of Steps";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox2.Location = new System.Drawing.Point(241, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "No of Walks";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1362, 650);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ RandomWalk2D/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomWalk2D
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Red);
+            SolidBrush sb1 = new SolidBrush(Color.Blue);
+            SolidBrush sb2 = new SolidBrush(Color.DarkGreen);
+
+            int nsteps = int.Parse(textBox1.Text);
+            int nwalks = int.Parse(textBox2.Text);
+
+            //Declearing 2D Array
+            float[] r2ave = new float[nsteps];
+            float y, x, delx = 10, dely = 10;  // x & y = dispalcement , delx & dely = step size
+            
+            Random obj = new Random();
+            double prob;
+
+            //Strating the Walk
+            for (int w = 0; w < nwalks; w++)
+            {
+                x = 0;
+                y = 0;
+
+                for (int s = 0; s < nsteps; s++)
+                {
+                    prob = obj.NextDouble();
+                    if (prob < 0.25)
+                    {
+                        x = x + delx;
+                    }
+                    if (prob >= 0.25 && prob < 0.5)
+                    {
+                        x = x - delx;
+                    }
+                    if (prob >= 0.5 && prob < 0.75)
+                    {
+                        y = y + dely;
+                    }
+                    if (prob >= 0.75)
+                    {
+                        y = y - dely;
+                    }
+
+                    gg.FillEllipse(sb2, 600 + x, 300 - y, 4, 4);
+                    
+
+                    r2ave[s] = r2ave[s] + x * x + y * y;
+                    //gg.FillEllipse(sb2, 400 + x, 400 - y, 10, 10);
+                    Thread.Sleep(10);
+
+                    //this.Refresh();
+                    //After each step the form will refresh
+
+                } // Ending of Single Walk
+            } // Ending of All Walks
+
+            //this.Refresh();
+            //After walks the form will refresh
+
+            // Computing the Mean Sqaure r
+            for (int s = 0; s < nsteps; s++)
+            {
+                r2ave[s] = r2ave[s] / nwalks;
+                // By this we will get averag displacement of every walkers
+
+                gg.FillEllipse(sb, 100 + s * 10, 600 - r2ave[s], 4, 4);
+                gg.FillEllipse(sb1, 100 + s * 10, 600 - (float)Math.Sqrt(r2ave[s]), 4, 4);
+            }
+        }
+    }
+}
